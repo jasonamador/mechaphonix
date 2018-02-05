c82783153d47ca05381f627350e2e7c98f724c0e
Gavin Loughridge <gavinlough@gmail.com>
2018-02-04 20:35:36 -0600

    added comments and removed console logs

--- mind-control-test/mind-app.js
+++ mind-control-test/mind-app.js
@@ -1,18 +1,16 @@
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
+// set the serial port bellow to whatever port your arduino is on
 const port = new SerialPort('/dev/tty.usbmodem1451');
 const parser = new Readline();
+// set the host bellow to whatever port your server is listening on
 const socket = require('socket.io-client')('http://localhost:3000');
 
-socket.on('play', function(data){
-  console.log('recived:', data);
-})
-
 socket.on('connect', function(){
-  console.log('connected to server');
   port.pipe(parser);
   parser.on('data', function(raw) {
     eeg = raw.trim().split(',')
+
     if (eeg.length == 11) {
       socket.emit('eeg data', {
         source: 'eeg',
@@ -27,13 +25,7 @@ socket.on('connect', function(){
         highBeta: eeg[8],
         lowGamma: eeg[9],
         highGamma: eeg[10]
-      });
+      })
     }
-  });
-  socket.on('summary', function(data){
-    console.log('******');
-    console.log('last count:', data.count - 10);
-    console.log('this count:', data.count);
-    console.log('next count:', data.count + 10);
   })
 });
--- mind-control-test/mind-server.js
+++ mind-control-test/mind-server.js
@@ -3,25 +3,13 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let summary = {
-  attention: [],
-  meditation: [],
-  delta: [],
-  theta: [],
-  lowAlpha: [],
-  highAlpha: [],
-  lowBeta: [],
-  highBeta: [],
-  lowGamma: [],
-  highGamma: [],
-  count: 0
-}
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/site.html');
+  res.sendFile(__dirname + '/mind-site.html');
 });
 
+// function to translate raw eeg data into notes for tone.js
 function getNote(number) {
   if (number < 2) return 'a5'
   if (number < 4) return 'g5'
@@ -29,8 +17,8 @@ function getNote(number) {
   if (number < 8) return 'e5'
   if (number < 10) return 'd5'
   if (number < 20) return 'c5'
-  if (number < 40) return 'b4'
-  if (number < 60) return 'a4'
+  if (number < 30) return 'b4'
+  if (number < 40) return 'a4'
   if (number < 80) return 'g4'
   if (number < 100) return 'f4'
   if (number < 200) return 'e4'
@@ -55,13 +43,13 @@ function getNote(number) {
   if (number < 1000000) return 'g1'
   if (number < 2000000) return 'f1'
   if (number < 4000000) return 'e1'
-  if (number < 6000000) return 'd1'
-  if (number < 8000000) return 'c1'
+  return 'd1'
 }
 
 io.on('connection', function(socket){
   console.log('connected to client');
   socket.on('eeg data', function(eeg){
+    console.log(eeg);
     let notes = [
       // getNote(eeg.attention)
       getNote(eeg.delta),
@@ -74,60 +62,12 @@ io.on('connection', function(socket){
       getNote(eeg.highGamma)
     ]
 
-    console.log('data is', eeg);
-    console.log('sending notes', notes);
-
     socket.broadcast.emit('play', {
       notes: notes
     })
-    // summary.attention.push(eeg.attention)
-    // summary.meditation.push(eeg.meditation)
-    // summary.delta.push(eeg.delta)
-    // summary.theta.push(eeg.theta)
-    // summary.lowAlpha.push(eeg.lowAlpha)
-    // summary.highAlpha.push(eeg.highAlpha)
-    // summary.lowBeta.push(eeg.lowBeta)
-    // summary.highBeta.push(eeg.highBeta)
-    // summary.lowGamma.push(eeg.lowGamma)
-    // summary.highGamma.push(eeg.highGamma)
-    // summary.count += 1
-    //
-    // if (summary.count % 10 == 0) {
-    //   console.log('***** summary for count ', (summary.count - 10), '*****');
-    //   console.log('attention',(summary.count - 10),Math.max(...summary.attention),Math.floor(summary.attention.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.attention))
-    //   console.log('meditation',(summary.count - 10),Math.max(...summary.meditation),Math.floor(summary.meditation.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.meditation))
-    //   console.log('delta',(summary.count - 10),Math.max(...summary.delta),Math.floor(summary.delta.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.delta))
-    //   console.log('theta',(summary.count - 10),Math.max(...summary.theta),Math.floor(summary.theta.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.theta))
-    //   console.log('lowAlpha',(summary.count - 10),Math.max(...summary.lowAlpha),Math.floor(summary.lowAlpha.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.lowAlpha))
-    //   console.log('highAlpha',(summary.count - 10),Math.max(...summary.highAlpha),Math.floor(summary.highAlpha.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.highAlpha))
-    //   console.log('lowBeta',(summary.count - 10),Math.max(...summary.lowBeta),Math.floor(summary.lowBeta.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.lowBeta))
-    //   console.log('highBeta',(summary.count - 10),Math.max(...summary.highBeta),Math.floor(summary.highBeta.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.highBeta))
-    //   console.log('lowGamma',(summary.count - 10),Math.max(...summary.lowGamma),Math.floor(summary.lowGamma.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.lowGamma))
-    //   console.log('highGamma',(summary.count - 10),Math.max(...summary.highGamma),Math.floor(summary.highGamma.reduce((p,c,_,a) => p + c/a.length,0)),Math.min(...summary.highGamma))
-    //
-    //   let count = summary.count
-    //
-    //   summary = {
-    //     attention: [],
-    //     meditation: [],
-    //     delta: [],
-    //     theta: [],
-    //     lowAlpha: [],
-    //     highAlpha: [],
-    //     lowBeta: [],
-    //     highBeta: [],
-    //     lowGamma: [],
-    //     highGamma: [],
-    //     count: count
-    //   }
-    //
-    //   io.emit('summary', {
-    //     count: count
-    //   })
-    // }
   });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
