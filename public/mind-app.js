a93f57ee53a19a74aef5cbe443c3fdf604bd8218
Gavin Loughridge <gavinlough@gmail.com>
2018-02-06 10:26:29 -0600

    added mind-app to master

---
+++ public/mind-app.js
@@ -0,0 +1,31 @@
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+// set the serial port bellow to whatever port your arduino is on
+const port = new SerialPort('/dev/tty.usbmodem1451');
+const parser = new Readline();
+// set the host bellow to whatever port your server is listening on
+const socket = require('socket.io-client')('http://localhost:3000');
+
+socket.on('connect', function(){
+  port.pipe(parser);
+  parser.on('data', function(raw) {
+    eeg = raw.trim().split(',')
+
+    if (eeg.length == 11) {
+      socket.emit('eeg input', {
+        source: 'eeg',
+        signal: eeg[0],
+        attention: eeg[1],
+        meditation: eeg[2],
+        delta: eeg[3],
+        theta: eeg[4],
+        lowAlpha: eeg[5],
+        highAlpha: eeg[6],
+        lowBeta: eeg[7],
+        highBeta: eeg[8],
+        lowGamma: eeg[9],
+        highGamma: eeg[10]
+      })
+    }
+  })
+});
