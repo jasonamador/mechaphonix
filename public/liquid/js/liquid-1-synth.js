0b50b71734c0366de984946da75854b0c10870f1
Jason Amador <jason.amador@gmail.com>
2018-02-06 15:47:54 -0600

    some tweaks

--- public/liquid/js/liquid-1-synth.js
+++ public/liquid/js/liquid-1-synth.js
@@ -5,9 +5,9 @@ SOUNDS
 /*
 Some adapter stuff
 */
-let degrees = [1, 3, 4, 5, 7, 8, 12, 13];
+let degrees = [1, 2, 3, 5, 6, 8, 9, 10];
 let scalePattern = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24];
-let notes = ['a1', 'a#1', 'b1', 'c2', 'c#2', 'd2', 'd#2', 'e2', 'f2', 'f#2', 'g2', 'g#2', 'a2', 'a#2', 'b2', 'c3', 'c#3', 'd3', 'eb3', 'e3', 'f3', 'f#3', 'g3', 'g#3', 'a3', 'a#3', 'b3', 'c4'];
+let notes = ['a1', 'a#1', 'b1', 'c2', 'c#2', 'd2', 'd#2', 'e2', 'f2', 'f#2', 'g2', 'g#2', 'a2', 'a#2', 'b2', 'c3', 'c#3', 'd3', 'eb3', 'e3', 'f3', 'f#3', 'g3', 'g#3', 'a3', 'a#3', 'b3', 'c4', 'c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4', 'g4', 'g#4', 'a4', 'a#4', 'b4', 'c5'];
 
 let blue = {
   source : new Tone.Noise('white'),
@@ -29,7 +29,7 @@ let red = {
   prevNote : '',
   update : function(message) {
     this.panner.pan.value = message.x - 0.5;
-    this.note = notes[scalePattern[degrees[Math.floor(message.y * 6)] + 2] + 4];
+    this.note = notes[scalePattern[degrees[Math.floor(message.y * 6)] - 1] + 3 + 12];
     this.source.triggerAttackRelease(this.note, '2n');
     this.prevNote = this.note;
   },
@@ -45,7 +45,7 @@ let green = {
   prevNote : '',
   update : function(message) {
     this.panner.pan.value = message.x - 0.5;
-    this.note = notes[scalePattern[degrees[Math.floor(message.y * 8)]] + 4];
+    this.note = notes[scalePattern[degrees[Math.floor(message.y * 8)] - 1] + 8 + 12];
     this.source.triggerAttackRelease(this.note, '2n');
     this.prevNote = this.note;
   },
