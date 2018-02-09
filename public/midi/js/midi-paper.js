c75815569f4d694b85002838d1effb1b757fea75
Christopher Castellano <c.castellano18@gmail.com>
2018-02-09 13:42:24 -0600

    paper stuff

--- public/midi/js/midi-paper.js
+++ public/midi/js/midi-paper.js
@@ -1,28 +1,28 @@
 paper.install(window);
-let x1= 720;
-let y1=350;
+
+
+
 let path;
-let r = 0.5;
-let g = 0.5;
-let b = 0.5;
-let color = new Color(r, g, b);
+
+let pt1 =(670, 300);
+let pt2 =(670, 400);
+let pt3 =(570, 400);
+let pt4 =(570, 300);
 window.onload = function(){
 console.log("window loaded");
   paper.setup("myCanvas");
 
-  path =new Path.Circle(new Point(x1,y1), 50);
- path.fillColor=color;
-console.log(path)
+path =new Path();
+path.strokeColor = 'black';
+path.add(new Point(pt1));
+path.add(new Point(400, 670));
+path.add(new Point(570, 400));
+path.add(new Point(570, 300));
+
+path.closed = true;
 
 paper.view.draw()
 }
-  // {
-  //   center: new Point(100, 100),
-  //   radius: 30,
-  //   strokeColor:'black'
-  // });
-
-
 
 // MIDI signal controller
 function midiPaper(midi) {
@@ -31,15 +31,23 @@ function midiPaper(midi) {
   console.log(midi.note)
   console.log(path)
   if(midi.cc === 224){
-    if(midi.vel > 64){
-      path.position = new Point(x1,(y1-midi.vel));
-    }
+    // if(midi.vel > 64){
+    //   path.position = new Point(300,(400-(midi.vel + 2)));
+    // }
+    // if(midi.vel < 64){
+    //   path.position = new Point(x,(y1 - (midi.vel +2)));
+    // }
   }
-  if(midi.cc === 153 && midi.note === 50 && midi.vel > 0){
-      color.r =  0.5;
-  }
-  if(midi.cc == 153 && midi.note === 45 && midi.vel > 0){
-
-  }
+  // if(midi.cc === 176){
+  //   path.position = new Point((x1 - midi.vel), y1)
+  // }
+  //
+  // if(midi.cc === 176 &&)
+  // if(midi.cc === 153 && midi.note === 50 && midi.vel > 0){
+  //   path.colorFill = "blue"
+  // }
+  // if(midi.cc == 153 && midi.note === 45 && midi.vel > 0){
+  //
+  // }
 
 }
