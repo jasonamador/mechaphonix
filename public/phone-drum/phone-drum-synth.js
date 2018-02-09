410b1c98b6a9dd479d4812a5d740a32915820c41
Gavin Loughridge <gavinlough@gmail.com>
2018-02-09 10:33:17 -0600

    removed triangle sample

--- public/phone-drum/phone-drum-synth.js
+++ public/phone-drum/phone-drum-synth.js
@@ -5,8 +5,7 @@ const sampler = new Tone.Sampler({
   'b4' : '/samples/cowbell.wav',
   'c4' : '/samples/clap.wav',
   'd4' : '/samples/large_drum.mp3',
-  'e4' : '/samples/snare.wav',
-  'f4' : '/samples/triangle.mp3'
+  'e4' : '/samples/snare.wav'
 }, () => {
   socket.on('phone drum message', (message) => {
     sampler.triggerAttack(message.note);
