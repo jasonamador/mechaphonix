9f20b1eb35aa47244b464cbe938cec4f58e2ba39
Jason Amador <jason.amador@gmail.com>
2018-02-23 16:22:58 -0600

    port env var

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
@@ -72,6 +73,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on port 3000');
+http.listen(port, function(){
+  console.log('listening on port ' + port);
 });
